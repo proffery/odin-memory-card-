959da05bda449e2ae2faa9256362c7242c01617f
Dmitry Shamko <proffery@gmail.com>
2023-05-15 17:10:01 +0300

    created react app

---
+++ src/components/screens/start/Start.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import styles from './Start.module.css';
+
+const Start = () => {
+
+  return (
+    <div className={styles.container}>
+      Hello, World!
+    </div>
+  );
+};
+
+export default Start;
