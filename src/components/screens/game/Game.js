187c8da8d6d84c540f39aceffbbacae7213a83cc
Dmitry Shamko <proffery@gmail.com>
2023-05-19 14:09:14 +0300

    game logic

--- src/components/screens/game/Game.js
+++ src/components/screens/game/Game.js
@@ -4,25 +4,32 @@ import { Planets } from '../../../Planets'
 import Card from './card/Card'
 
 const Game = (prop) => {
-
+    const [cards, setCards] = useState(new Array(prop.prop.round + 1))
     const [updateScore, setUpdateScore] = useState(prop.prop.score)
     const [updateBestScore, setUpdateBestScore] = useState(prop.prop.bestScore)
     const [updateRound, setUpdateRound] = useState(prop.prop.round)
 
-    const handleScore = () => {
-        setUpdateScore(prev => prev + 1)
-        prop.onChangeScore(updateScore)
-    }
+        // const handleScore = () => {
+        //     setUpdateScore(prev => prev + 1)
+        //     prop.onChangeScore(updateScore)
+        // }
+    
+        // const handleBestScore = () => {
+        //     setUpdateBestScore(prev => prev + 1)
+        //     prop.onChangeBestScore(updateBestScore)
+        // }
+    
+        // const handleRound = () => {
+        //     setUpdateRound(prev => prev + 1)
+        //     prop.onChangeRound(updateRound)
+        // }
+        const getRandomId = () => {
+            const rand = Math.floor(Math.random() * (Planets.length)).toString()
+            return rand
+        }
+        cards.forEach(card => {setCards(prev => [...prev,  Planets.filter(planet => planet.id === getRandomId())])}) 
+        console.log(cards)
 
-    const handleBestScore = () => {
-        setUpdateBestScore(prev => prev + 1)
-        prop.onChangeBestScore(updateBestScore)
-    }
-
-    const handleRound = () => {
-        setUpdateRound(prev => prev + 1)
-        prop.onChangeRound(updateRound)
-    }
 
     return (
         <div className={styles.container}>
@@ -30,7 +37,7 @@ const Game = (prop) => {
             <button onClick={handleScore}>Increment score</button>
             <button onClick={handleRound}>Increment round</button>
             <button onClick={handleBestScore}>Increment Best score</button> */}
-            {Planets.map(planet =>
+            {cards.map(planet =>
                 <Card key={'pl'+ planet.id} id={planet.id} name={planet.name} className={styles.card}/>
             )}
 
